3a5b251ba51d9910a6e6136edf73fded253b2547
Artem_Sazonov <artem.sazonov.dev@gmail.com>
2026-07-20 20:34:18 +0400

    chore: add project dependencies

---
+++ src/main/java/ru/emobile/tinyurl/TinyUrlServiceApplication.java
@@ -0,0 +1,9 @@
+package ru.emobile.tinyurl;
+
+@SpringBootApplication
+public class TinyUrlServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyUrlServiceApplication.class, args);
+    }
+}
